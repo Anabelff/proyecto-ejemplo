6c3f5b359215e33df42462725288981b092686c1
anabelff <unisys.a fernande@iberia.es>
2023-01-12 18:33:03 +0100

    CAmbiado a mundo

--- src/main/java/com/unisys/Main.java
+++ src/main/java/com/unisys/Main.java
@@ -2,6 +2,6 @@ package com.unisys;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola world!");
+        System.out.println("Hola mundo!");
     }
 }
