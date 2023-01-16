d3e12e9206038df1a3464542ad70af966bdbe348
anabelff <unisys.a fernande@iberia.es>
2023-01-16 18:11:39 +0100

    Ejercio de Variables

---
+++ src/main/java/com/unisys/ejercicios/Variables.java
@@ -0,0 +1,28 @@
+package com.unisys.ejercicios;
+
+public class Variables {
+
+    public static void main ( String[] args) {
+        int numero=1;
+        short marianico=23;
+        long gasol=1000;
+        boolean independencia=true;
+        double presupuesto =10.00;
+        float longitud = 14.4f;
+        long suma;
+
+        suma = gasol +marianico;
+
+        System.out.println("El valor de numero es " +numero ) ;
+        System.out.println("El valor de marianico es " +marianico ) ;
+        System.out.println("El valor de gasol es " +gasol ) ;
+        System.out.println("El valor de independencia es " +independencia ) ;
+        System.out.println("El valor de presupuesto es " +presupuesto ) ;
+        System.out.println("El valor de longitud es " +longitud ) ;
+
+        System.out.println("El valor de suma es " +suma);
+
+    }
+
+
+}
