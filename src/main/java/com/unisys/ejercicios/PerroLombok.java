01421f3e2b39a368458f8c979ccc9c0de8317fab
anabelff <unisys.a fernande@iberia.es>
2023-01-19 16:11:50 +0100

    Incluimos un atributo tipo Date a la clase PerroLombok

--- src/main/java/com/unisys/ejercicios/PerroLombok.java
+++ src/main/java/com/unisys/ejercicios/PerroLombok.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PerroLombok {
     private String dueno;
     private String edad;
+    private Date fecha;
 }
--- src/main/java/com/unisys/ejercicios/PerroLombokApp.java
+++ src/main/java/com/unisys/ejercicios/PerroLombokApp.java
@@ -1,12 +1,15 @@
 package com.unisys.ejercicios;
 
+import java.util.Date;
+
 public class PerroLombokApp {
     public static void main(String[] args) {
         PerroLombok perrito= new PerroLombok();
         perrito.setDueno("Anabel");
         perrito.setEdad("99");
+        perrito.setFecha(new Date(2004,10,04));
         System.out.println(perrito);
-        PerroLombok chucho =new PerroLombok("Susana","10");
+        PerroLombok chucho =new PerroLombok("Susana","10",new Date());
         System.out.println(chucho);
     }
 
